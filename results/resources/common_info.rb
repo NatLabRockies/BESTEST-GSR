835e6885e5d5bbc143c1b36b2279269c4b65de23
David Goldwasser <david.f.goldwasser@gmail.com>
2026-06-09 08:58:20 -0600

    updating org info and running tests
    Will make release soon for OS 11.

--- results/resources/common_info.rb
+++ results/resources/common_info.rb
@@ -6,18 +6,18 @@ module BestestResults
     hash = {}
 
     if program == "OS"
-      hash[:program_name_and_version] = "OpenStudio 3.10.0"
-      hash[:program_version_release_date] = "06/18/2025"
+      hash[:program_name_and_version] = "OpenStudio 3.11.0"
+      hash[:program_version_release_date] = "01/15/6"
       hash[:program_name_short] = "OS"
     else
-      hash[:program_name_and_version] = "EnergyPlus 25.1.0 , Bug Fix Edition"
-      hash[:program_version_release_date] = "05/28/25"
+      hash[:program_name_and_version] = "EnergyPlus 25.2.0 , Bug Fix Edition"
+      hash[:program_version_release_date] = "11/14/25"
       hash[:program_name_short] = "E+"
     end
 
-    hash[:results_submission_date] = "06/18/2025"
-    hash[:organization] = "National Renewable Energy Laboratory"
-    hash[:organization_short] = "NREL"
+    hash[:results_submission_date] = "06/09/2026"
+    hash[:organization] = "National Laboratory of the Rockies"
+    hash[:organization_short] = "NLR"
 
     return hash
 
